a3f90227a782337b86877fad414c4962dd88a9d8
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-08-27 19:55:02 -0700

    Basic Node server - closes #10

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '../client/static'));
+app.use(bodyParser.json());
+app.use(express.static(_dirname + '../client/static'));
 
 app.listen(port);
-console.log('listening on ' + port);
 
 exports = module.exports = app;
